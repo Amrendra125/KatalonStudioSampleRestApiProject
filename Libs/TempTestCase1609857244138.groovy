70dbe683f57bb73f5103830441f443a7b9f987db
“emb-kumaramr“ <amrendra.kumar.extern@diconium.com>
2021-01-06 11:17:15 +0530

    SampleRestAPI Chaining Demo script

---
+++ Libs/TempTestCase1609857244138.groovy
@@ -0,0 +1,22 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\EMB-AM~1\\AppData\\Local\\Temp\\Katalon\\Test Cases\\APIChainingDemo\\20210105_200404\\execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/APIChainingDemo', new TestCaseBinding('Test Cases/APIChainingDemo',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
